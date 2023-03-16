f510c1cbc745e5a78b4941e358e0bd883117347d
youssef <ybenchiir@gmail.com>
2023-03-16 14:43:43 -0700

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-/* This is The entry point */
+/**
+ * main - Entry point 
+ * Description: 'function serves as the starting point for program execution'
+ * @parameter: no parameters
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
