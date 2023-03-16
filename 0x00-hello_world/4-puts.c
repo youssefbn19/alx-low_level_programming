f8316c9555ce496fddb81eb26069b195d553dc1b
youssef <ybenchiir@gmail.com>
2023-03-16 14:49:47 -0700

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
  * main - Entry point 
  * Description: 'function serves as the starting point for program execution'
- * @parameter: no parameters
+ * @void: no parameters
  * Return: Always 0 (Success)
  */
 int main(void)
