371bd785c5691e69433cfc461062e429545d4fec
youssef <ybenchiir@gmail.com>
2023-03-17 00:35:57 -0700

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
  * main - Entry point 
- * Description: function serves as the starting point for program execution
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -9,4 +8,3 @@ int main(void)
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-
