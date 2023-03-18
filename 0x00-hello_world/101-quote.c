bb1907795b92da048c1a49ae3da56b9488419c84
youssef <ybenchiir@gmail.com>
2023-03-18 01:14:08 -0700

    Add 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+/**
+ * main - Entry point Print out a string in the standard error.
+ *Return: 1 if success.
+*/
+int main(void)
+
+{
+	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, quote, strlen(quote));
+	return (1);
+}
