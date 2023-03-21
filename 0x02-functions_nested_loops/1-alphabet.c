2c71a944a2f3a4eff918de37f731585c6fb7059d
youssef <ybenchiir@gmail.com>
2023-03-21 10:21:31 -0700

    Update main.h & Add 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_alphabet - Print the alphabet
+ *
+ *
+ *
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+
+}
