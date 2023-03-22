6e50eedd3c27331c8781c2eea588dea79c4228b3
youssef <ybenchiir@gmail.com>
2023-03-22 11:24:39 -0700

    Add 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,14 +9,15 @@ int main(void)
 	long int fib, num1, num2, sum;
 	int i;
 
-	num1 = sum = 0;
+	num1 = 0;
+	sum = 0;
 	num2 = 1;
 	for (i = 1; i <= 50; i++)
 	{
 		fib = num1 + num2;
 		num1 = num2;
 		num2 = fib;
-		if ((fib % 10) == 0)
+		if ((fib % 2) == 0 && fib <= 4000000)
 			sum += fib;
 	}
 	printf("%ld\n", sum);
