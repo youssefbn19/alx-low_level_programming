df3f6d4f9b25861322445d7a6bb80b5e552e5151
youssef <ybenchiir@gmail.com>
2023-03-23 08:24:25 -0700

    Add 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- *print_numbers - prints the number from 0 to 9
+ *print_most_numbers - prints the number from 0 to 9 except 2 and 4
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i;
 
