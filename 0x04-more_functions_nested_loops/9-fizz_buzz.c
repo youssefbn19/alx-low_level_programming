bfe8de752596486e7cd8b1e0f508540958d5c904
youssef <ybenchiir@gmail.com>
2023-03-24 04:05:16 -0700

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,21 +18,22 @@ int main(void)
 	{
 		if ((i % 3) == 0 && (i % 5) == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if ((i % 3) == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if ((i % 5) == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else
 		{
 			printf("%d ", i);
 		}
 	}
+
 	putchar('\n');
 	return (0);
 }
