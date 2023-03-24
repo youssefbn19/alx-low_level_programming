0f67fb7eff537949fc9dd64f6edf71ff59648ac9
youssef <ybenchiir@gmail.com>
2023-03-24 08:19:43 -0700

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,30 +1,27 @@
 #include <stdio.h>
 /**
- *
- *
- *
- *
+ * main - print the largest prime factor of the number 612852475143
+ * Return: 0 always if success
  */
 int main(void)
 {
-	long long int pf_sum, num, i, new_num, lpf;
+	long int pf_sum, num, i, new_num, lpf;
 
 	num = new_num = 612852475143;
 	pf_sum = 1;
 	i = 2;
 	while (pf_sum != num)
 	{
-		if ((new_num % i) == 0)
-		{
-			pf_sum *= i;
-			lpf = i;
-			new_num /= i;
-		}
-		else
+		if ((new_num % i) != 0)
 		{
 			i++;
+			continue;
 		}
+
+		pf_sum *= i;
+		lpf = i;
+		new_num /= i;
 	}
-	printf("%lld\n", lpf);
+	printf("%ld\n", lpf);
 	return (0);
 }
