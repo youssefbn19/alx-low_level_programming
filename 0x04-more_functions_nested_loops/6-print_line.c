377bdbc463f64c76c74eb458c049a3e37596868d
youssef <ybenchiir@gmail.com>
2023-03-24 03:01:17 -0700

    Update 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *print_line - print straight line(s)
+ *@n: integer number
+ *Description: print a number of straight depends on
+ *the argument number (n)
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
