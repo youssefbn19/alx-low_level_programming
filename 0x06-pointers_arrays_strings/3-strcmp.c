1f1a66711900e891c2da9d9b122bffcc7ec04289
youssef <ybenchiir@gmail.com>
2023-03-31 08:54:20 -0700

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,14 +1,22 @@
 /**
+ *_strcmp - compare two string
  *
+ *@s1: first string
+ *@s2: second string
  *
- *
- *
- *
- *
- *
+ *Return: return positive integer if s1 > s2 and negative integer otherwise
+ *and 0 if they are equal
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	return (s1[0] - s2[0]);
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+
+	return (0);
 }
