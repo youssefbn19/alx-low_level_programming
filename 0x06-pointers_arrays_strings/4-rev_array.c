0e8ac25a0a68d754d4c2c8dbb67b6f5dc4ebdf4b
youssef <ybenchiir@gmail.com>
2023-03-31 09:08:22 -0700

    Update 4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+/**
+ *reverse_array - reverse an array
+ *
+ *@a: an array of integer
+ *@n: the number of elements of the array
+ *
+ *
+ */
+
+void reverse_array(int *a, int n)
+{
+	int swp, i, ln;
+
+	ln = n - 1; 
+	for (i = 0; i <= ln; i++)
+	{
+		swp = a[i];
+		a[i] = a[ln];
+		a[ln] = swp;
+		ln--;
+	}
+}
