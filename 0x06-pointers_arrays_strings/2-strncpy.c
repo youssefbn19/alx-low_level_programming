da3f288cae69b40803b9851c5d423709c9dca427
youssef <ybenchiir@gmail.com>
2023-03-31 08:21:22 -0700

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,16 +1,22 @@
 /**
+ *_strncpy - Copy a string
  *
+ *@dest: first string pointer
+ *@src: second string pointer
+ *@n: integer number
  *
- *
+ *Return: edited string pointer `dest`
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	int len = sizeof(dest);
-	
-	for (i = 0; i < n; i++)
-		dest[i] = src[i];	
+	int len = sizeof(dest) - 1;
 
-	
+	for (i = 0; (i < n && src[i] != '\0'); i++)
+		dest[i] = src[i];
+
+	for (; i < n; i++)
+		dest[i] = '\0';
+
 	return (dest);
 }
