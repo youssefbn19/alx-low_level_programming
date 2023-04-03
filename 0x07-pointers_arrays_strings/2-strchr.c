440ace517f3603c520f482cb53f9012ca91814d0
youssef <ybenchiir@gmail.com>
2023-04-03 09:14:55 -0700

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -14,11 +14,8 @@ char *_strchr(char *s, char c)
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
-			break;
+			return (s + i);
 	}
 
-	if (s[i] == '\0')
-		return (0);
-
-	return (s + i);
+	return ('\0');
 }
