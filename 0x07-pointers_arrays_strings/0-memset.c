65bc46cabf1791293c7d50a583c68183724bdd8c
youssef <ybenchiir@gmail.com>
2023-04-03 07:54:34 -0700

    Add 0x07-pointers_arrays_strings

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+/**
+ *_memset - fills memory with a constant byte character;
+ *
+ *@s: string to fills with the constant byte
+ *@b: constant byte character
+ *@n: number of bytes
+ *
+ *Return: a pointer to the memory area `s`
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n && s[i] != '\0'; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+# ALX Project: 0x07. C - Even more pointers, arrays and strings
