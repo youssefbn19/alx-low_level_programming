199dc17929b57e61a97afe42fcccb49e0f2f45e3
youssef <ybenchiir@gmail.com>
2023-04-13 07:05:01 -0700

    Add 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,11 +1,10 @@
 #include <stdlib.h>
 /**
+ * malloc_checked - create an allocate memory space
  *
+ * @b: allocated memory size
  *
- *
- *
- *
- *
+ * Return: a pointer to the allocated memory;
  */
 void *malloc_checked(unsigned int b)
 {
