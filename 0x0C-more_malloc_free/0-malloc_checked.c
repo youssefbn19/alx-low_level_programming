8b2b0f7d5620e40dc9c0537872a34524e166d2e2
youssef <ybenchiir@gmail.com>
2023-04-13 07:00:08 -0700

    Add 0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
