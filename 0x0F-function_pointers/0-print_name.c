d3e4fa693de8547dfa29f22a75f00ad82d2c8ed1
youssef <ybenchiir@gmail.com>
2023-04-19 09:06:29 -0700

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-#include <stdio.h>
 /**
  * print_name - prints a name
  *
@@ -8,5 +7,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name != NULL && f != NULL)
 		f(name);
 }
