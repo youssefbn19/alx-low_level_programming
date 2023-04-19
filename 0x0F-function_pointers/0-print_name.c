213c3c03edd1f220f15cd96374bb8d70e7ded575
youssef <ybenchiir@gmail.com>
2023-04-19 09:58:20 -0700

    Add 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * print_name - prints a name with text depends on function
+ *
+ * @name: name that want to show
+ * @f: pointer to function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
