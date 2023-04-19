1a382b1ff029d50c5452294cbc0445b8d6081f7f
youssef <ybenchiir@gmail.com>
2023-04-19 08:38:04 -0700

    Add 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints a name
+ *
+ * @name: name
+ * @f: pointer to function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
