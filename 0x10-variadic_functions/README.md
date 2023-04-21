a33cef82b94cc175cfaa9453d26d58a3be5d883a
youssef <ybenchiir@gmail.com>
2023-04-21 08:04:24 -0700

    Add README.md, variadic_functions.h

---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+# ALX Project : 0x10. C - Variadic functions
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,8 @@
+#ifndef VA_FUNC
+#define VA_FUNC
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+#endif
