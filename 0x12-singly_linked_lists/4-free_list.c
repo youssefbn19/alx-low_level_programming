01d6725d237c3dcfd322839f6f305b28b7100a58
youssef <ybenchiir@gmail.com>
2023-04-28 07:33:01 -0700

    Add 4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_list - frees the allocation memory of a linked list
+ * @head: linked list node pointer
+ */
+void free_list(list_t *head)
+{
+	if (head == NULL)
+		return;
+
+	list_t *temp = head;
+
+	while (head)
+	{
+		head = head->next;
+		free(temp->str);
+		free(temp);
+		temp = head;
+	}
+}
