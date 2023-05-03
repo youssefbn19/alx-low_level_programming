7cfbe3e5af2549acae05d9490c661c7beeec6f87
youssef <ybenchiir@gmail.com>
2023-05-03 04:33:06 -0700

    Update 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -2,6 +2,55 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
+ * last_node - searches a listint_t linked list cycle for its last node
+ *
+ * @head: pointer to the first node in a listint_t linked list
+ * @detect: pointer to the detected cycle pointer
+ *
+ * Return: pointer to the last node in the listint_t linked ist
+ *
+ */
+const listint_t *last_node(const listint_t *head, const listint_t *detect)
+{
+	const listint_t *temp = NULL;
+
+	temp = head;
+	while (temp->next != detect->next)
+	{
+		temp = temp->next;
+		detect = detect->next;
+	}
+
+	return (detect);
+}
+/**
+ * detect_cycle - checks if a listint_t linked list has a cycle
+ *
+ * @head: pointer to the first node in a listint_t linked list
+ *
+ * Return: pointer if a cycle is detected, NULL if not detected
+ */
+const listint_t *detect_cycle(const listint_t *head)
+{
+	const listint_t *slow_node = NULL, *fast_node = NULL;
+
+	if (head == NULL)
+		return (NULL);
+
+	slow_node = fast_node = head;
+
+	while (slow_node && fast_node && fast_node->next)
+	{
+		slow_node = slow_node->next;
+		fast_node = fast_node->next->next;
+
+		if (slow_node == fast_node)
+			return (slow_node);
+	}
+
+	return (NULL);
+}
+/**
  * print_listint_safe - prints a listint_t linked list
  *
  * @head: pointer to the first node in a listint_t linked list
@@ -10,38 +59,35 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *curr = NULL, *fast_node = NULL;
-	size_t i = 0;
+	const listint_t *temp = NULL, *detect = NULL, *last_node = NULL;
+	size_t len = 0;
 
 	if (head == NULL)
-		return (i);
+		return (0);
 
-	curr = fast_node = head;
-	while (curr)
+	temp = head;
+	detect = detect_cycle(head);
+	if (detect == NULL)
 	{
-		printf("[%p] %d\n", (void *)curr, curr->n);
-		i++;
-		curr = curr->next;
-		if (fast_node->next && fast_node->next->next)
+		while (temp)
 		{
-			fast_node = fast_node->next->next;
-			if (curr == fast_node)
-			{
-				if (curr != curr->next->next)
-				{
-					printf("[%p] %d\n", (void *)curr, curr->n);
-					i++;
-				}
-				curr = curr->next;
-				printf("[%p] %d\n", (void *)curr, curr->n);
-				i++;
-
-				printf("-> [%p] %d\n", (void *)curr->next, curr->next->n);
-				exit(98);
-			}
+			printf("[%p] %d\n", (void *)temp, temp->n);
+			temp = temp->next;
+			len++;
 		}
-
+	}
+	else
+	{
+		last_node = last_node(head, detect);
+		while (temp != last_node)
+		{
+			printf("[%p] %d\n", (void *)temp, temp->n);
+			temp = temp->next;
+		}
+		printf("[%p] %d\n", (void *)temp, temp->n);
+		printf("->[%p] %d\n", (void *)(last_node->next), (last_node->next->n));
+		exit(98);
 	}
 
-	return (i);
+	return (len);
 }
