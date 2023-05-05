f7230a6fa973473c845bbba074d187d44c828824
youssef <ybenchiir@gmail.com>
2023-05-05 13:44:13 -0700

    Fix the error

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,7 +11,7 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int pos = 0;
-	int max_index = sizeof(unsigned long int) * 8;
+	unsigned int max_index = sizeof(unsigned long int) * 8;
 
 	if (index > max_index)
 		return (-1);
