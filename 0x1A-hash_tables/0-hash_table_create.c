043d668a5aed26a18be8443a5e363c8b8b4df3dc
youssef <ybenchiir@gmail.com>
2023-07-08 07:57:33 -0700

    First commit

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create -  creates a hash table.
+ *
+ * @size: hash table size
+ *
+ * Return: a pointer to the newly created hash table,
+ * NULL if something goes wrong
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_t = NULL;
+	unsigned long int i = 0;
+
+	if (size == 0)
+	{
+		fprintf(stderr, "Size must be greater than zero");
+		return (NULL);
+	}
+
+	hash_t = malloc(sizeof(hash_table_t));
+	if (hash_t == NULL)
+	{
+		fprintf(stderr, "Can't allocate memory");
+		return (NULL);
+	}
+
+	hash_t->size = size;
+	hash_t->array = malloc(sizeof(hash_node_t *) * size);
+	for (; i < size; i++)
+		hash_t->array[i] = NULL;
+
+	return (hash_t);
+}
+
