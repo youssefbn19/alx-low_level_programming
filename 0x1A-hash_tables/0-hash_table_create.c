1efe60ec7cb56e26ba29404c5fb26c95e115ea3e
youssef <ybenchiir@gmail.com>
2023-07-08 08:20:41 -0700

    Update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,26 +12,22 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *hash_t = NULL;
 	unsigned long int i = 0;
 
+	if (size == 0)
+		return (NULL);
+
 	hash_t = malloc(sizeof(hash_table_t));
 	if (hash_t == NULL)
-	{
-		fprintf(stderr, "Can't allocate memory");
 		return (NULL);
-	}
 
 	hash_t->size = size;
-	if (size == 0)
-		return (hash_t);
 
 	hash_t->array = malloc(sizeof(hash_node_t *) * size);
 	if (hash_t->array == NULL)
-	{
-		fprintf(stderr, "Can't allocate memory");
 		return (NULL);
-	}
 
 	for (; i < size; i++)
 		hash_t->array[i] = NULL;
+
 	return (hash_t);
 }
 
