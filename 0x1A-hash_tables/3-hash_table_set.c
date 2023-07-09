f1581ff1cc9b39299d68757e3ecc7f139b975cfd
youssef <ybenchiir@gmail.com>
2023-07-09 07:39:50 -0700

    Add insert function

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,55 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set -  adds an element to the hash table.
+ *
+ * @ht: The hash table you want to add or update the key/value to.
+ * @key: String key.
+ * @value: The value associated with the key.
+ *
+ * Return: 1 if it succeeded, 0 otherwise.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int indx = 0;
+	hash_node_t *node = NULL, *temp = NULL;
+
+	if (ht == NULL)
+		return (0);
+	else if (key == NULL || value == NULL)
+		return (0);
+
+	indx = key_index((const unsigned char *)key, ht->size);
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (0);
+
+	node->key = malloc(sizeof(key) * (strlen(key) + 1));
+	node->value = malloc(sizeof(value) * (strlen(value) + 1));
+	strcpy(node->key, key);
+	strcpy(node->value, value);
+	node->next = NULL;
+
+	if (ht->array[indx] == NULL)
+		ht->array[indx] = node;
+	else
+	{
+		temp = ht->array[indx];
+		while (temp)
+		{
+			if (strcmp(temp->key, key) == 0)
+			{
+				temp->value = realloc(temp->value, (strlen(value) + 1));
+				strcpy(temp->value, value);
+				free(node);
+				break;
+			}
+			else if (temp->next == NULL)
+			{
+				temp->next = node;
+				break;
+			}
+			temp = temp->next;
+		}
+	}
+	return (1);
+}
