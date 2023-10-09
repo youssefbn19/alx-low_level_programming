49cd1c91187a022850433410e5491663fea9bfba
Youssef Benchiir <ybenchiir@gmail.com>
2023-10-09 18:34:51 +0100

    Add min and jump_search functions

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,61 @@
+#include "search.h"
+#include <math.h>
+#include <stdio.h>
+/**
+ * min - Checks for the small number
+ *
+ * @num1: first number.
+ * @num2: second number.
+ *
+ * Return: The small number
+ */
+size_t min(size_t num1, size_t num2)
+{
+	if (num1 < num2)
+		return (num1);
+	else
+		return (num2);
+}
+/**
+ * jump_search - Searches for a value in a sorted array of integers using
+ * the Jump search algorithm
+ *
+ * @array: A pointer to the first element of the array to search in.
+ * @size: The number of elements in array.
+ * @value: The value to search for.
+ *
+ * Return: The index where value is located, otherwise return -1 if
+ * value is not present in array or if array is NULL.
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	size_t start, jump_step;
+
+	if (array)
+	{
+		start = 0;
+		jump_step = 0;
+		while (array[min(jump_step, size)] < value)
+		{
+			start = jump_step;
+			printf("Value checked array[%ld] = [%d]\n", start, array[start]);
+			jump_step += (int)sqrt(size);
+			if (start >= size)
+				return (-1);
+		}
+		printf("Value found between indexes [%ld] and [%ld]\n", start, jump_step);
+		while (array[start] < value)
+		{
+			printf("Value checked array[%ld] = [%d]\n", start, array[start]);
+			if (start == min(jump_step, size))
+				return (-1);
+			start++;
+		}
+		if (array[start] == value)
+		{
+			printf("Value checked array[%ld] = [%d]\n", start, array[start]);
+			return (start);
+		}
+	}
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,4 +5,6 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array_elements(int *array, size_t start, size_t end);
+int jump_search(int *array, size_t size, int value);
+size_t min(size_t num1, size_t num2);
 #endif
